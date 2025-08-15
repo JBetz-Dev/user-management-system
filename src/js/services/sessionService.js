9b7ec576b77d2b7f9557b56fb2c95ba7f5e87afd
JBetz-Dev <98231502+JBetz-Dev@users.noreply.github.com>
2025-08-14 21:04:43 -0400

    feat: add session service for user session management

---
+++ src/js/services/sessionService.js
@@ -0,0 +1,26 @@
+import {SESSION_STORAGE_KEYS} from "../utils/constants";
+
+class SessionService {
+
+    getActiveSession() {
+        const userData = JSON.parse(sessionStorage.getItem(SESSION_STORAGE_KEYS.USER));
+
+        if (userData && userData.active === true) {
+            return userData;
+        }
+
+        return null;
+    }
+
+    setActiveSession(userData) {
+        this.clearActiveSession();
+        userData.active = true;
+        sessionStorage.setItem(SESSION_STORAGE_KEYS.USER, JSON.stringify(userData));
+    }
+
+    clearActiveSession() {
+        sessionStorage.removeItem(SESSION_STORAGE_KEYS.USER);
+    }
+}
+
+export const sessionService = new SessionService();
